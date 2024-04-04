b30bdf6a216e9e17daa4d6d1f42a521d074e044f
kimdonghwan <1489ehdghks@gmail.com>
2024-04-04 09:36:51 +0900

    준성님 코드까지 완료

--- app.py
+++ app.py
@@ -16,10 +16,12 @@ login_manager.login_view = 'login'
 
 # DB 설정
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'database.db')
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 db = SQLAlchemy(app)
+
 
 # User 모델
 class User(UserMixin, db.Model):
@@ -34,6 +36,7 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 # Post 모델
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -45,9 +48,19 @@ class Post(db.Model):
     image_path = db.Column(db.String(100), nullable=True)
     user = db.relationship('User', backref=db.backref('posts', lazy=True))
 
+
+# 좋아요 모델
+class Likes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    liked = db.Column(db.Boolean, default=False)
+
+
 # DB 생성
 with app.app_context():
     db.create_all()
+
 
 # 회원가입
 @app.route('/register', methods=['GET', 'POST'])
@@ -62,7 +75,8 @@ def register():
 
         hashed_password = generate_password_hash(password)
 
-        new_user = User(username=username, userID=userID, password_hash=hashed_password)
+        new_user = User(username=username, userID=userID,
+                        password_hash=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         flash('회원가입에 성공하였습니다!', 'success')
@@ -70,11 +84,32 @@ def register():
 
     return render_template('register.html')
 
-# 로그인
+
+# 로그인관련
 @login_manager.user_loader
 def load_user(userID):
     return User.query.get(int(userID))
 
+
+# 홈
+@app.route("/")
+def home():
+    user_list = User.query.all()
+    post_list = Post.query.all()
+
+    # Likes 리스트
+    like_list = []
+    if current_user.is_active:
+        like_list = Likes.query.filter_by(
+            user_id=current_user.id, liked=1).all()
+    # likes_list = Likes.query.all() #일단 비어있고, 로그인 시에만
+    print(like_list)
+    likes_list = [like.post_id for like in like_list]
+    print(likes_list)
+    return render_template('index.html', user=user_list, posts=post_list, likes=likes_list)
+
+
+# 로그인
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -95,6 +130,7 @@ def login():
 
     return render_template('index.html')
 
+
 # 로그아웃
 @app.route('/logout')
 @login_required
@@ -102,13 +138,15 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+
 # 이미지 업로드
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-@app.route("/posts/create", methods=['GET','POST'])
+
+@app.route("/posts/create", methods=['GET', 'POST'])
 @login_required
-def home():
+def home1():
     if request.method == 'POST':
         post_content = request.form.get('content')
         image_file = request.files.get('imageUpload')
@@ -127,8 +165,8 @@ def home():
                 db.session.commit()
                 flash('새 포스트가 성공적으로 생성되었습니다.', 'success')
             except Exception as e:
-                # Log the error for better debugging
-                app.logger.error(f"Error during image upload or database save: {str(e)}")
+                app.logger.error(
+                    f"Error during image upload or database save: {str(e)}")
                 flash('이미지 업로드 또는 데이터베이스 저장 중 오류가 발생했습니다.', 'error')
         else:
             flash('올바른 이미지 파일을 업로드해주세요.', 'error')
@@ -142,18 +180,26 @@ def home():
     post_list = Post.query.all()
     return render_template('index.html', user=user_list, posts=post_list)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+# 포스트 삭제
+@app.route("/posts/delete/<int:post_id>", methods=["DELETE"])
+def post_delete(post_id):
+    post = Post.query.get_or_404(post_id)
+    try:
+        print("post_id:", post_id)
+        db.session.delete(post)
+        db.session.commit()
+        return jsonify({"success": "Post deleted successfully"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 # 포스트 수정
-
 @app.route("/posts/edit/<int:post_id>", methods=["PATCH"])
 def edit_post(post_id):
-    data = request.get_json()  # JSON 형식의 요청 본문을 파싱
+    data = request.get_json()
     updated_content = data.get('content')
-
-    # 데이터베이스에서 해당 포스트를 찾아서 내용을 업데이트합니다.
+    print("edit NOW", post_id)
     post = Post.query.get(post_id)
     if post:
         post.content = updated_content
@@ -161,3 +207,28 @@ def edit_post(post_id):
         return jsonify({"success": "Post updated successfully"}), 200
     else:
         return jsonify({"error": "Post not found"}), 404
+
+
+# 좋아요 기능 구현
+@app.route("/addLike/<int:post_id>", methods=["POST"])
+@login_required
+def add_like(post_id):
+    post = Post.query.get_or_404(post_id)
+    like = Likes.query.filter_by(
+        user_id=current_user.id, post_id=post_id).first()
+    if not like:
+        like = Likes(user_id=current_user.id, post_id=post_id, liked=True)
+        db.session.add(like)
+        post.likes += 1
+    else:
+        if like.liked:
+            post.likes -= 1
+        else:
+            post.likes += 1
+        like.liked = not like.liked
+    db.session.commit()
+    return jsonify({"success": "Like added successfully", "current_likes": post.likes})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
